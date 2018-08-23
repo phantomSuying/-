a829a5616aa90c0b3d7a23602477da98fa3256a5
phantomSuying <40844291+phantomSuying@users.noreply.github.com>
2018-08-22 23:25:41 -0700

    Add files via upload

--- ClientPart.java
+++ ClientPart.java
@@ -1,15 +1,11 @@
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.arxanfintech.common.crypto.Crypto;
-import com.arxanfintech.common.rest.Api;
 import com.arxanfintech.common.rest.Client;
-import com.arxanfintech.common.rest.Common;
-import com.arxanfintech.common.rest.Request;
 import com.arxanfintech.sdk.wallet.Wallet;
 import org.apache.commons.codec.binary.Base64;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -51,7 +47,7 @@ public class ClientPart {
 //                "User226",
 //                "first"));
         try {
-            String response = clientPart.wallet.QueryPOE(clientPart.header, "did:axn:577b2073-882f-41ab-8603-114f75d0217a").getString("Payload");
+            String response = clientPart.wallet.QueryPOE(clientPart.header, "did:axn:a20b40ff-ccff-4f7a-a839-acbde84b6a6e").getString("Payload");
             System.out.println(clientPart.analyzeMetadata(JSON.parseObject(response).getString("metadata")));
         } catch (Exception e) {
             e.printStackTrace();
@@ -62,6 +58,23 @@ public class ClientPart {
 //            System.out.println(clientPart.updatePOE(jsonObject,
 //                    "User226",
 //                    "first"));
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        try {
+//            JSONObject jsonPayload = new JSONObject();
+//            jsonPayload.put("id", "");
+//            jsonPayload.put("name", "PermissionBox");
+//            jsonPayload.put("owner", clientPart.client.GetCreator());
+//            jsonPayload.put("hash", "");
+//            jsonPayload.put("metadata", "");
+//            System.out.println(clientPart.wallet.CreatePOE(clientPart.header,
+//                    jsonPayload,
+//                    clientPart.client.GetCreator(),
+//                    null,
+//                    null,
+//                    clientPart.client.GetPrivateB64(),
+//                    "C:/Go/src/github.com/arxanchain/sdk-go-common/crypto/tools/build/bin/sign-util.exe"));
 //        }catch (Exception e){
 //            e.printStackTrace();
 //        }
@@ -83,7 +96,7 @@ public class ClientPart {
         jsonData.put("id", "did:axn:kwsxz" + access);//在access前加上did:axn:kwsxz作为前缀当作ID
         try {
             JSONObject response = wallet.Register(this.header, jsonData);
-            JSONObject jsonKeyPair = (JSONObject) JSON.parseObject((String) response.get("Payload")).get("key_pair");
+            JSONObject jsonKeyPair = JSON.parseObject(response.getString("Payload")).getJSONObject("key_pair");
             JSONObject jsonMessage = new JSONObject();
             jsonMessage.put("key_pair", jsonKeyPair);
             jsonMessage.put("POEdid", "");
@@ -91,8 +104,8 @@ public class ClientPart {
             return response;
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
     /**
@@ -141,8 +154,8 @@ public class ClientPart {
             return resp;
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
     /**
@@ -157,8 +170,8 @@ public class ClientPart {
             return jsonObject.getJSONObject(access);
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
     /**
@@ -188,16 +201,16 @@ public class ClientPart {
                     this.getMessageBoxInformation().getJSONObject(access).getJSONObject("key_pair").getString("private_key"),
                     "C:/Go/src/github.com/arxanchain/sdk-go-common/crypto/tools/build/bin/sign-util.exe"
             );
-            String rePayload = (String) response.get("Payload");
-            String POEdid = (String) JSON.parseObject(rePayload).get("id");
-            JSONObject reMessage = (JSONObject) this.getMessageBoxInformation().get(access);
+            String rePayload = response.getString("Payload");
+            String POEdid = JSON.parseObject(rePayload).getString("id");
+            JSONObject reMessage = this.getMessageBoxInformation().getJSONObject(access);
             reMessage.put("POEdid", POEdid);
             this.updateMessageBox(access, reMessage);
             return response;
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
     /**
@@ -220,14 +233,7 @@ public class ClientPart {
             jsonPayload.put("owner", "did:axn:kwsxz" + access);
             jsonPayload.put("metadata", encodedText);
 
-            JSONObject response = this.wallet.UpdatePOE(this.header,
-                    jsonPayload,
-                    this.client.GetCreator(),
-                    null,
-                    null,
-                    this.getMessageBoxInformation().getJSONObject(access).getJSONObject("key_pair").getString("private_key"),
-                    "C:/Go/src/github.com/arxanchain/sdk-go-common/crypto/tools/build/bin/sign-util.exe");
-//            Request request = new Request();
+            //            Request request = new Request();
 //            request.client = this.client;
 //            request.body = Common.BuildBody(jsonPayload,
 //                    "did:axn:kwsxz" + access,
@@ -242,41 +248,73 @@ public class ClientPart {
 //            Api api = new Api();
 //            api.NewHttpClient();
 //            String response = api.DoPut(request);
-            return response;
+            return this.wallet.UpdatePOE(this.header,
+                    jsonPayload,
+                    this.client.GetCreator(),
+                    null,
+                    null,
+                    this.getMessageBoxInformation().getJSONObject(access).getJSONObject("key_pair").getString("private_key"),
+                    "C:/Go/src/github.com/arxanchain/sdk-go-common/crypto/tools/build/bin/sign-util.exe");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 查询信息许可，查看哪些人向自己发来了请求信息
+     *
+     * @param selfAccess 用户名
+     * @return 查询者用户名
+     */
+    public JSONArray getPermissionFromWho(String selfAccess) {
+        try {
+            JSONObject jsonObject = this.getPermissionBoxInformation();
+            if (jsonObject.containsKey(selfAccess)) {
+                return jsonObject.getJSONArray(selfAccess);
+            } else {
+                return new JSONArray();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * @param jsonArray  请求访问的用户列表
+     * @param selfAccess 自己的用户名
+     */
+    public void updatePermissionBox(JSONArray jsonArray, String selfAccess) {
+        try {
+            JSONObject jsonObject = this.getPermissionBoxInformation();
+            jsonObject.put(selfAccess, jsonArray);
+            String string = this.JSONObjectToMetadata(jsonObject);
+
+            JSONObject jsonPayload = new JSONObject();
+            jsonPayload.put("id", "did:axn:a20b40ff-ccff-4f7a-a839-acbde84b6a6e");
+            jsonPayload.put("name", "PermissionBox");
+            jsonPayload.put("owner", this.client.GetCreator());
+            jsonPayload.put("hash", "");
+            jsonPayload.put("metadata", string);
+
+            System.out.println(this.wallet.UpdatePOE(this.header,
+                    jsonPayload,
+                    this.client.GetCreator(),
+                    null,
+                    null,
+                    this.client.GetPrivateB64(),
+                    "C:/Go/src/github.com/arxanchain/sdk-go-common/crypto/tools/build/bin/sign-util.exe"));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
-    }
-
-    /**
-     * 寻求许可
-     *
-     * @param otherAccess 他人用户名
-     * @param selfAccess  自己的用户名
-     * @return 返回结果信息，以String存储，需要转换为json数据结构，返回值需判断返回代码是否为零
-     */
-    public JSONObject askForPermission(String selfAccess, String otherAccess) {
-        return null;
-    }
-
-    /**
-     * 查询信息许可
-     *
-     * @param access 用户名
-     * @return 许可信息，用JSON数据结构存储
-     */
-    public String getPermission(String access) {
-        return null;
     }
 
     /**
      * @param access      用户名
      * @param jsonPayload 用户信息
      */
-    private void updateMessageBox(String access, JSONObject jsonPayload) {
-        JSONObject jsonHeader = new JSONObject();
-        jsonHeader.put("Bc-Invoke-Mode", "sync");
+    public void updateMessageBox(String access, JSONObject jsonPayload) {
         try {
             JSONObject jsonMetadataMessage = this.getMessageBoxInformation();
             jsonMetadataMessage.put(access, jsonPayload);
@@ -285,16 +323,16 @@ public class ClientPart {
             JSONObject jsonPOEPayload = new JSONObject();
             jsonPOEPayload.put("id", "did:axn:af7380cb-2137-4c2d-a98e-62aba671f6df");
             jsonPOEPayload.put("name", "MessageBox");
-            jsonPOEPayload.put("owner", "did:axn:124d00f2-ea55-4724-8e58-31680d443628");
+            jsonPOEPayload.put("owner", this.client.GetCreator());
             jsonPOEPayload.put("hash", "");
             jsonPOEPayload.put("metadata", stringMetadataMessage);
 
-            System.out.println(this.wallet.UpdatePOE(jsonHeader,
+            System.out.println(this.wallet.UpdatePOE(this.header,
                     jsonPOEPayload,
-                    "did:axn:124d00f2-ea55-4724-8e58-31680d443628",
+                    this.client.GetCreator(),
                     null,
                     null,
-                    "2RPpCLAl0CNiiXMLjUNSC1acqtkvU8+U9MtU2yvo4Vz52m8mW4+UrvqmFosxi/pu/AzpFf+CCQtutYCtKOZFoQ==",
+                    this.client.GetPrivateB64(),
                     "C:/Go/src/github.com/arxanchain/sdk-go-common/crypto/tools/build/bin/sign-util.exe"));
         } catch (Exception e) {
             e.printStackTrace();
@@ -304,7 +342,7 @@ public class ClientPart {
     /**
      * @return 信息中心所有信息
      */
-    private JSONObject getMessageBoxInformation() {
+    public JSONObject getMessageBoxInformation() {
         try {
             JSONObject jsonObject = this.wallet.QueryPOE(this.header, "did:axn:af7380cb-2137-4c2d-a98e-62aba671f6df");
             String rePayload = (String) jsonObject.get("Payload");
@@ -312,15 +350,73 @@ public class ClientPart {
             return this.analyzeMetadata(metadataMessage);
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
+    }
+
+    /**
+     * @param selfAccess  自己用户名，若是允许访问，用户名前面为ON，若不允许，用户名前为OFF
+     * @param otherAccess 要查询用户的用户名
+     * @return 若为真则可访问，若为否则未被同意访问或暂时未被同意访问
+     */
+    public boolean askPermissionBox(String selfAccess, String otherAccess) {
+        try {
+            JSONObject jsonMetadataMessage = this.getPermissionBoxInformation();
+            if (jsonMetadataMessage.containsKey(otherAccess)) {
+                if (jsonMetadataMessage.getJSONArray(otherAccess).contains("OFF" + selfAccess)) {
+                    return false;
+                } else if (jsonMetadataMessage.getJSONArray(otherAccess).contains("ON" + selfAccess)) {
+                    return true;
+                } else {
+                    jsonMetadataMessage.getJSONArray(otherAccess).add("OFF" + selfAccess);
+                    jsonMetadataMessage.put(otherAccess, jsonMetadataMessage.getJSONArray(otherAccess));
+                }
+            } else {
+                JSONArray jsonArray = new JSONArray();
+                jsonArray.add("OFF" + selfAccess);
+                jsonMetadataMessage.put(otherAccess, jsonArray);
+            }
+            String stringMetadataMessage = this.JSONObjectToMetadata(jsonMetadataMessage);
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("id", "did:axn:a20b40ff-ccff-4f7a-a839-acbde84b6a6e");
+            jsonObject.put("name", "PermissionBox");
+            jsonObject.put("owner", this.client.GetCreator());
+            jsonObject.put("hash", "");
+            jsonObject.put("metadata", stringMetadataMessage);
+
+            System.out.println(this.wallet.UpdatePOE(this.header,
+                    jsonObject,
+                    this.client.GetCreator(),
+                    null,
+                    null,
+                    this.client.GetPrivateB64(),
+                    "C:/Go/src/github.com/arxanchain/sdk-go-common/crypto/tools/build/bin/sign-util.exe"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * @return 查询中心所有信息
+     */
+    private JSONObject getPermissionBoxInformation() throws Exception {
+        JSONObject jsonObject = this.wallet.QueryPOE(this.header, "did:axn:a20b40ff-ccff-4f7a-a839-acbde84b6a6e");
+        String rePayload = jsonObject.getString("Payload");
+        String metadataMessage = JSON.parseObject(rePayload).getString("metadata");
+        return this.analyzeMetadata(metadataMessage);
     }
 
     /**
      * @param metadataString 待解码数据
      * @return 解码后数据
      */
-    private JSONObject analyzeMetadata(String metadataString) {
+    private JSONObject analyzeMetadata(String metadataString) throws Exception {
+        if (metadataString == null) {
+            throw new Exception();
+        }
         Base64 base64 = new Base64();
         byte[] bytes = base64.decode(metadataString);
         return (JSONObject) JSON.parse(bytes);
@@ -330,15 +426,10 @@ public class ClientPart {
      * @param jsonObject 待编码数据
      * @return 编码后数据
      */
-    private String JSONObjectToMetadata(JSONObject jsonObject) {
-        try {
-            String string = jsonObject.toJSONString();
-            byte[] bytes = string.getBytes("UTF-8");
-            Base64 base64 = new Base64();
-            return base64.encodeToString(bytes);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    private String JSONObjectToMetadata(JSONObject jsonObject) throws Exception {
+        String string = jsonObject.toJSONString();
+        byte[] bytes = string.getBytes("UTF-8");
+        Base64 base64 = new Base64();
+        return base64.encodeToString(bytes);
     }
 }
